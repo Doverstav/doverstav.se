a736ca3eb4c0c1364df172c8c6f9a05e649471d4
Doverstav <pontus.doverstav@kits.se>
2018-10-24 21:46:53 +0200

    Add delay between resetting and creating new shooting stars

--- src/app/objects/ShootingStar.ts
+++ src/app/objects/ShootingStar.ts
@@ -8,6 +8,8 @@ export class ShootingStar {
     private x: number;
     private y: number;
 
+    private timedOut: boolean;
+
     constructor(backgroundHeight: number, backgroundWidth: number) {
         this.height = backgroundHeight;
         this.width = backgroundWidth;
@@ -19,18 +21,27 @@ export class ShootingStar {
         this.x = Math.random() * this.width;
         this.y = 0;
 
-        this.size = (Math.random() * 1) + 0.1;
+        this.size = Math.random() + 0.1;
         this.speed = (Math.random() * 10) + 6;
         this.length = (Math.random() * 80) + 10;
     }
 
     update() {
-        this.x = this.x - this.speed;
-        this.y = this.y + this.speed;
+        if(!this.timedOut) {
+            this.x = this.x - this.speed;
+            this.y = this.y + this.speed;
 
-        // If out of bounds, reset
-        if(this.x < 0 || this.y > this.height) {
-            this.initShootingStar();
+            // If out of bounds, reset
+            if(this.x < 0 || this.y > this.height) {
+                // Set shooting star as timedOut so it wont keep updating
+                this.timedOut = true;
+                // Reset star now so it line doesn't linger on canvas
+                this.initShootingStar()
+                // Reset timeOut status in some time
+                setTimeout(
+                    () => this.timedOut = false, (Math.random() * 5000) + 1000
+                )
+            }
         }
     }
 
