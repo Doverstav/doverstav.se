a722412ac387888acaf30af9763bfd4a3ae3a5b6
Doverstav <pontus.doverstav@kits.se>
2018-10-29 21:28:37 +0100

    Set color of subtitle depending on selected palette

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,9 +1,18 @@
 import { CanvasRenderer } from "./canvasRender/CanvasRenderer";
+import { Palette } from "./util/Palette";
+import { PaletteUtil } from "./util/PaletteUtil";
 
 export class Main {
     main() {
         let canvasRenderer = new CanvasRenderer(window.innerHeight, window.innerWidth);
         canvasRenderer.renderCanvas();
+        this.setInfoboxColors();
+    }
+
+    setInfoboxColors() {
+        let palette: Palette = PaletteUtil.getPalette();
+        let infoSubtitle: HTMLElement = document.querySelector('.info-subtitle');
+        infoSubtitle.style.color = palette.bot;
     }
 }
 
