f4db0d4e7d7131c512c3fd3dff0ee92c07ee3ec0
Doverstav <pontus.doverstav@kits.se>
2018-10-30 21:06:39 +0100

    Add comments, change button text when panel is hidden/shown

--- src/app/main.ts
+++ src/app/main.ts
@@ -17,23 +17,34 @@ export class Main {
     }
 
     setButtonEventListener() {
+        // Find button in document
         let actionButton: HTMLElement = document.querySelector('button#hideButton');
-        console.log(actionButton);
-        actionButton.onclick = (event) => this.handleActionButtonClick(event); 
+        // Set onclick listener
+        actionButton.onclick = (event) => this.handleActionButtonClick(event, actionButton); 
     }
 
-    handleActionButtonClick(event: MouseEvent) {
+    handleActionButtonClick(event: MouseEvent, button: HTMLElement) {
         let mainElement = document.querySelector('main');
         if(mainElement.classList.contains('hide-panel-animation')) {
+            // User requested to show panel
+            // Remove css classes for hiding panel
             mainElement.classList.remove('hide-panel-animation');
             mainElement.classList.remove('main-margin-hidden');
+            // Add css classes for showing panel
             mainElement.classList.add('main-margin-top');
             mainElement.classList.add('show-panel-animation');
+            // Set button text
+            button.innerText = 'Hide infopanel';
         } else {
+            // User requested to hide panel
+            // Remove css classes for showing panel
             mainElement.classList.remove('show-panel-animation');
             mainElement.classList.remove('main-margin-top');
+            // Add css classes for hiding panel
             mainElement.classList.add('hide-panel-animation');
             mainElement.classList.add('main-margin-hidden');
+            // Set button text
+            button.innerText = 'Show infopanel';
         }
     }
 }
