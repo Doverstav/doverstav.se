757ffbe45e90038f64763f1aba0d54907af91579
Doverstav <pontus.doverstav@kits.se>
2018-10-30 20:33:29 +0100

    Implement PoC of animation for hiding infopanel

--- src/app/main.ts
+++ src/app/main.ts
@@ -7,12 +7,34 @@ export class Main {
         let canvasRenderer = new CanvasRenderer(window.innerHeight, window.innerWidth);
         canvasRenderer.renderCanvas();
         this.setInfoboxColors();
+        this.setButtonEventListener();
     }
 
     setInfoboxColors() {
         let palette: Palette = PaletteUtil.getPalette();
         let infoSubtitle: HTMLElement = document.querySelector('.info-subtitle');
         infoSubtitle.style.color = palette.bot;
+    }
+
+    setButtonEventListener() {
+        let actionButton: HTMLElement = document.querySelector('button#actionButton');
+        console.log(actionButton);
+        actionButton.onclick = (event) => this.handleActionButtonClick(event); 
+    }
+
+    handleActionButtonClick(event: MouseEvent) {
+        let mainElement = document.querySelector('main');
+        if(mainElement.classList.contains('test-animation')) {
+            mainElement.classList.remove('test-animation');
+            mainElement.classList.remove('main-margin-hidden');
+            mainElement.classList.add('main-margin-top');
+            mainElement.classList.add('test-animation-revert');
+        } else {
+            mainElement.classList.remove('test-animation-revert');
+            mainElement.classList.remove('main-margin-top');
+            mainElement.classList.add('test-animation');
+            mainElement.classList.add('main-margin-hidden');
+        }
     }
 }
 
